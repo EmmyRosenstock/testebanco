6587914209cd68132c234bb7377b0bb6b6e7a163
EmmyRosenstock <emusca@hotmail.com>
2023-09-05 23:56:46 -0300

    bdatualizado

--- app.js
+++ app.js
@@ -1,105 +1,161 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
-const bodyParser = require('body-parser');
-const { v4: uuidv4 } = require('uuid');
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000; 
 
-app.use(bodyParser.json());
-
-
-const db = new sqlite3.Database('controle_transacoes.db');
+const db = new sqlite3.Database('contas.db');
 
 
+app.use(express.json());
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
 db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS pessoas (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT NOT NULL
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS cartoes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      numero TEXT NOT NULL,
+      pessoa_id INTEGER,
+      FOREIGN KEY (pessoa_id) REFERENCES pessoas(id)
+    )
+  `);
+
   db.run(`
     CREATE TABLE IF NOT EXISTS contas (
-      id TEXT PRIMARY KEY,
-      cliente_nome TEXT,
-      cliente_numero_cartao TEXT,
-      saldo REAL
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      numero TEXT NOT NULL,
+      pessoa_id INTEGER,
+      FOREIGN KEY (pessoa_id) REFERENCES pessoas(id)
     )
   `);
-});
 
-
-db.serialize(() => {
   db.run(`
-    CREATE TABLE IF NOT EXISTS transacoes (
-      id TEXT PRIMARY KEY,
-      conta_id TEXT,
-      tipo TEXT,
-      valor REAL,
-      data_criacao TEXT,
-      FOREIGN KEY (conta_id) REFERENCES contas (id)
+    CREATE TABLE IF NOT EXISTS tipos_transacao (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT NOT NULL
     )
   `);
+
+  
 });
-
-
-app.post('/contas', (req, res) => {
-  const { cliente_nome, cliente_numero_cartao } = req.body;
-  //if (!cliente_nome || !cliente_numero_cartao) {
-    //return res.status(400).json({ erro: 'Cliente_nome e cliente_numero_cartao são obrigatórios' });
-  //}
-
-  const id = uuidv4();
-  const saldo = 0;
-
-  db.run('INSERT INTO contas (id, cliente_nome, cliente_numero_cartao, saldo) VALUES (?, ?, ?, ?)', [id, cliente_nome, cliente_numero_cartao, saldo], (err) => {
+// Rota para criar um novo cartão para um portador existente
+app.post('/cartoes', (req, res) => {
+  const { numero, pessoa_id } = req.body;
+  
+ 
+  db.get('SELECT * FROM pessoas WHERE id = ?', [pessoa_id], (err, row) => {
     if (err) {
-      console.error(`Erro ao criar conta: ${err}`);
-      return res.status(500).json({ erro: 'Erro ao criar conta' });
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+      return;
     }
-
-    res.status(201).json({ id, cliente_nome, cliente_numero_cartao, saldo });
-  });
-});
-
-
-app.post('/transacoes', (req, res) => {
-  const { tipo, valor, conta_id } = req.body;
- // if (!tipo || !valor || !conta_id) {
-    //return res.status(400).json({ erro: 'Tipo, valor e conta_id são obrigatórios' });
-  //}
-
-  const data_criacao = new Date().toISOString();
-  const id = uuidv4();
-
-  db.run('INSERT INTO transacoes (id, conta_id, tipo, valor, data_criacao) VALUES (?, ?, ?, ?, ?)', [id, conta_id, tipo, valor, data_criacao], (err) => {
-    if (err) {
-      console.error(`Erro ao registrar transação: ${err}`);
-      return res.status(500).json({ erro: 'Erro ao registrar transação' });
+    if (!row) {
+      res.status(404).json({ error: 'Pessoa não encontrada' });
+      return;
     }
-
     
-    const query = tipo === 'compra' || tipo === 'saque' ? 'UPDATE contas SET saldo = saldo - ? WHERE id = ?' : 'UPDATE contas SET saldo = saldo + ? WHERE id = ?';
-    db.run(query, [valor, conta_id], (err) => {
+    db.run('INSERT INTO cartoes (numero, pessoa_id) VALUES (?, ?)', [numero, pessoa_id], function(err) {
       if (err) {
-        console.error(`Erro ao atualizar saldo da conta: ${err}`);
-        return res.status(500).json({ erro: 'Erro ao atualizar saldo da conta' });
+        console.error(err.message);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+        return;
       }
-
-      res.status(201).json({ id, tipo, valor, data_criacao });
+      res.json({ message: 'Cartão criado com sucesso' });
     });
   });
 });
 
-
-app.get('/contas/:conta_id', (req, res) => {
-  const conta_id = req.params.conta_id;
-
-  db.all('SELECT * FROM transacoes WHERE conta_id = ?', [conta_id], (err, transacoes) => {
+app.post('/compra', (req, res) => {
+  const { conta_id, valor, data } = req.body;
+  const tipo_transacao_id = 1; 
+  
+ 
+  db.run('INSERT INTO transacoes (conta_id, tipo_transacao_id, valor, data) VALUES (?, ?, ?, ?)', [conta_id, tipo_transacao_id, -valor, data], function(err) {
     if (err) {
-      console.error(`Erro ao buscar transações: ${err}`);
-      return res.status(500).json({ erro: 'Erro ao buscar transações' });
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+      return;
     }
-
-    res.json(transacoes);
+    res.json({ message: 'Transação de compra registrada com sucesso' });
   });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor está ouvindo na porta ${port}`);
+
+app.post('/saque', (req, res) => {
+  const { conta_id, valor, data } = req.body;
+  const tipo_transacao_id = 2; 
+  db.run('INSERT INTO transacoes (conta_id, tipo_transacao_id, valor, data) VALUES (?, ?, ?, ?)', [conta_id, tipo_transacao_id, -valor, data], function(err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+      return;
+    }
+    res.json({ message: 'Transação de saque registrada com sucesso' });
+  });
+});
+
+
+app.post('/pagamento', (req, res) => {
+  const { conta_id, valor, data } = req.body;
+  const tipo_transacao_id = 3; 
+  db.run('INSERT INTO transacoes (conta_id, tipo_transacao_id, valor, data) VALUES (?, ?, ?, ?)', [conta_id, tipo_transacao_id, valor, data], function(err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+      return;
+    }
+    res.json({ message: 'Transação de pagamento registrada com sucesso' });
+  });
+});
+
+app.post('/compra', (req, res) => {
+  const { conta_id, valor, data } = req.body;
+  const tipo_transacao_id = 1; 
+  
+
+  db.run('INSERT INTO transacoes (conta_id, tipo_transacao_id, valor, data) VALUES (?, ?, ?, ?)', [conta_id, tipo_transacao_id, -valor, data], function(err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+      return;
+    }
+    res.json({ message: 'Transação de compra registrada com sucesso' });
+  });
+});
+
+
+app.post('/saque', (req, res) => {
+  const { conta_id, valor, data } = req.body;
+  const tipo_transacao_id = 2; 
+  db.run('INSERT INTO transacoes (conta_id, tipo_transacao_id, valor, data) VALUES (?, ?, ?, ?)', [conta_id, tipo_transacao_id, -valor, data], function(err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+      return;
+    }
+    res.json({ message: 'Transação de saque registrada com sucesso' });
+  });
+});
+
+
+app.post('/pagamento', (req, res) => {
+  const { conta_id, valor, data } = req.body;
+  const tipo_transacao_id = 3; 
+  db.run('INSERT INTO transacoes (conta_id, tipo_transacao_id, valor, data) VALUES (?, ?, ?, ?)', [conta_id, tipo_transacao_id, valor, data], function(err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+      return;
+    }
+    res.json({ message: 'Transação de pagamento registrada com sucesso' });
+  });
 });
